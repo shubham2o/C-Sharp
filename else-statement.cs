fa6da0a18597afc69882fa582ad63ac71fdc26cc
shubham2o <shubhamsharma.ha@gmail.com>
2021-10-04 13:29:26 +0530

    else-statement added

---
+++ else-statement.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time=20;
+            if (time < 18)
+            {
+                Console.WriteLine("Good Day.");
+            }
+            else
+            {
+                Console.WriteLine("Good Evening.");
+            }
+        }
+    }
+}
