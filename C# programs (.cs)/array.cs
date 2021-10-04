492b04511237f25c2fc3181e03d805be99ce7943
shubham2o <shubhamsharma.ha@gmail.com>
2021-10-04 22:28:35 +0530

    array.cs added

---
+++ C# programs (.cs)/array.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cars= {"Volvo", "BMW", "Ford", "Mercedes", "Audi"};
+            Console.WriteLine(cars[0]);
+        }
+    }
+}
+
+
+
+
+
+// OUTPUT - Volvo
