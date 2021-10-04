6e58d2d481ce9f97701d4d52b6994fbc1360b302
shubham2o <shubhamsharma.ha@gmail.com>
2021-10-04 22:42:16 +0530

    array-length added

---
+++ C# programs (.cs)/array-length.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace C_Sharp
+{
+    class program 
+    {
+        static void Main(string[] args)
+        {
+            string[] cars= {"Volvo", "BMW", "Mercedes", "Tesla"};
+            Console.WriteLine(cars.Length);
+        }
+    }
+}
+
+
+
+
+
+// OUTPUT - 4
