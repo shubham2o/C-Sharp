23a2d5ba9ec2ee93e501a404c74a2ed4a4bf7bf8
shubham2o <shubhamsharma.ha@gmail.com>
2021-10-04 18:54:43 +0530

    while-loop added

---
+++ C# programs (.cs)/while-loop.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i < 5)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+    }
+}
+
