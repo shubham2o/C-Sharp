b3305a6a44f4b4d08c538cfc710f8d8e20b11a96
shubham2o <shubhamsharma.ha@gmail.com>
2021-10-04 19:54:52 +0530

    for-loop-1 added

---
+++ C# programs (.cs)/while-loop-1.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i=0; i<=10; i=i+2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+
+
+
+
+
+// OUTPUT 
+//   0
+//   2
+//   4
+//   6
+//   8
+//   10
