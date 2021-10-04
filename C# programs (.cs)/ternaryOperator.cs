50ce6a2caaa5f44ffa9af69a43ede0635537d25f
shubham2o <shubhamsharma.ha@gmail.com>
2021-10-04 15:53:54 +0530

    Ternary Operator - Output added

--- C# programs (.cs)/ternaryOperator.cs
+++ C# programs (.cs)/ternaryOperator.cs
@@ -4,11 +4,17 @@ namespace C_Sharp
 {
     class Program
     {
-        static void Main{string[] args}
+        static void Main(string[] args)
         {
             int time=20;
-            string result = (time < 18) ? "Good Day." : "Good Evening.";
+            string result=(time < 18) ? "Good Day." : "Good Evening";
             Console.WriteLine(result);
         }
     }
 }
+
+
+
+
+
+// OUTPUT - Good Evening
