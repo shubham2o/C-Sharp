6170d37577553184a257f54d134dadde2fa37804
shubham2o <shubhamsharma.ha@gmail.com>
2021-10-04 19:38:26 +0530

    for-loop added

---
+++ C# programs (.cs)/for-loop.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
