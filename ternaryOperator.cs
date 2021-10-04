842e21a9e7572c1a67de2d34bebcf31db5c20ffd
shubham2o <shubhamsharma.ha@gmail.com>
2021-10-04 14:07:29 +0530

    Short Hand if Else (Ternary Operator)

---
+++ ternaryOperator.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace C_Sharp
+{
+    class Program
+    {
+        static void Main{string[] args}
+        {
+            int time=20;
+            string result = (time < 18) ? "Good Day." : "Good Evening.";
+            Console.WriteLine(result);
+        }
+    }
+}
