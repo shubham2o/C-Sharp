a5208adf92ab2f89b52e2cdaebcf2f98088be5ea
shubham2o <shubhamsharma.ha@gmail.com>
2021-10-05 10:31:12 +0530

    return-values.cs added

---
+++ C# programs (.cs)/return-values.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace C_Sharp
+{
+    class Program
+    {
+        static int MyMethod(int x)
+        {
+            return 5 + x;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MyMethod(4));
+        }
+    }
+}
+
+
+
+
+
+// OUTPUT - 9
