42b6c8c8d0202a8804f0e1026559b4d1963a378e
shubham2o <shubhamsharma.ha@gmail.com>
2021-10-05 09:08:07 +0530

    call-a-method.cs added

---
+++ C# programs (.cs)/call-a-method.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace C_Sharp
+{
+    class program
+    {
+        static void MyMethod()
+        {
+            Console.WriteLine("Hello! My name is Shubham Sharma");
+        }
+
+        static void Main(string[] args)
+        {
+            MyMethod();
+        }
+    }
+}
+
+
+
+
+
+// OUTPUT
+//  Hello! My name is Shubham Sharma
