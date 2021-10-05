0cc1789165a3483d8c62d92e2ea221afd227433a
shubham2o <shubhamsharma.ha@gmail.com>
2021-10-05 10:46:20 +0530

    return-values-two-parameters.cs added

---
+++ C# programs (.cs)/return-values-two-parameters.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace C_Sharp
+{
+    class program
+    {
+        static int MyMethod(int x, int y)
+        {
+            return x + y;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MyMethod(5,4));
+        }
+    }
+}
+
+
+
+
+
+// OUTPUT - 9
