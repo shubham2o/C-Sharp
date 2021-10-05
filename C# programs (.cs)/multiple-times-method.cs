4c983b2906c53473e1de57a212b761416b6763d6
shubham2o <shubhamsharma.ha@gmail.com>
2021-10-05 09:16:21 +0530

    multiple-times-method.cs added

---
+++ C# programs (.cs)/multiple-times-method.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace C_Sharp
+{
+    class program
+    {
+        static void MultipleMethod()
+        {
+            Console.WriteLine("SS");
+        }
+
+        static void Main(string[] args)
+        {
+            MultipleMethod();
+            MultipleMethod();
+            MultipleMethod();
+            MultipleMethod();
+            MultipleMethod();
+        }
+    }
+}
+
+
+
+
+
+// OUTPUT
+//  SS
+//  SS
+//  SS
+//  SS
+//  SS
