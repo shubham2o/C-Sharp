c37aeb4bbf694b1a08279925a2868081aa97c817
shubham2o <shubhamsharma.ha@gmail.com>
2021-10-05 18:38:03 +0530

    class-members.cs added

---
+++ C# programs (.cs)/class-members.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace C_Sharp
+{
+    class Myclass
+    {
+        // Class members
+        string color="red";         // field
+        int myspeed=200;            // field
+        public void fullspeed()     // method
+        {
+            Console.WriteLine("The car is going fast as it can!");
+        }
+    }
+}
