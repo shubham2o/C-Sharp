ee1b59239734e0ffbab5a91522ef9f88c8c6db06
shubham2o <shubhamsharma.ha@gmail.com>
2021-10-07 15:33:05 +0530

    constructor.cs added

---
+++ C# programs (.cs)/constructor.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace C_Sharp
+{
+    // Create a Car class
+    class Car
+    {
+        public string model;         // Create a field
+
+        // Create a class constructor for the Car class
+        public Car()
+        {
+            model = "Mustang";      // Set the initial value for model
+        }
+
+        static void Main(string[] args)
+        {
+            Car Ford= new Car();     // Create an object of the Car Class (this will call the constructor)
+            Console.WriteLine(Ford.model);       // Print the value of model
+        }
+
+    }
+}
+
+
+
+
+
+// OUTPUT - Mustang
