dc4652a4b1f3e25ed70df0df32cc4503258754e4
shubham2o <shubhamsharma.ha@gmail.com>
2021-10-07 19:08:16 +0530

    get-set.cs added

---
+++ C# programs (.cs)/get-set.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace C_Sharp
+{
+    class Person
+    {
+        private string name;        // field
+        public string Name          // property
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person myObj= new Person();
+            myObj.Name="Liam";
+            Console.WriteLine(myObj.Name);
+        }
+    }
+}
+
+
+
+
+
+// OUTPUT - Liamb
