adc4adf2729456312bc6f860e72875cdcabea49f
shubham2o <shubhamsharma.ha@gmail.com>
2021-10-07 23:17:23 +0530

    enum.cs added

---
+++ C# programs (.cs)/enum.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace C_Sharp
+{
+    enum Level
+    {
+        Low,
+        Medium,
+        High
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Level myVar= Level.Medium;
+            Console.WriteLine(myVar);
+        }
+    }
+}
+
+
+
+
+
+// OUTPUT - Medium
